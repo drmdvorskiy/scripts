47f149347ee46a717f86153306c12cbd6e721c6b
drmdvorskiy <90637928+drmdvorskiy@users.noreply.github.com>
2023-01-27 14:37:39 +0300

    Update blue.sh

--- blue.sh
+++ blue.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-mark hold firmware-microbit-micropython firmware-microbit-micropython-dl nvidia-kernel-support nvidia-tesla-470-kernel-support && apt install -y firmware-*
+apt-mark hold firmware-microbit-micropython firmware-microbit-micropython-dl nvidia-kernel-support nvidia-tesla-470-kernel-support && apt install -y firmware-* && apt-mark unhold firmware-microbit-micropython firmware-microbit-micropython-dl nvidia-kernel-support nvidia-tesla-470-kernel-support
