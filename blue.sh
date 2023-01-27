78eaef8bc5d02c7ecafd5109489ddf4b2058d4dd
Dv <drm.dvorskiy@gmail.com>
2023-01-27 15:06:32 +0300

    й

--- blue.sh
+++ blue.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-apt-mark hold firmware-microbit-micropython firmware-microbit-micropython-dl nvidia-kernel-support nvidia-tesla-470-kernel-support && apt install -y firmware-* && apt-mark unhold firmware-microbit-micropython firmware-microbit-micropython-dl nvidia-kernel-support nvidia-tesla-470-kernel-support
+apt-mark hold firmware-microbit-micropython 
+apt-mark hold firmware-microbit-micropython-dl
+apt-mark hold nvidia-kernel-support
+apt-mark hold nvidia-tesla-470-kernel-support
+apt install -y firmware-* 
+apt-mark unhold firmware-microbit-micropython
+apt-mark unhold firmware-microbit-micropython-dl
+apt-mark unhold nvidia-kernel-support
+apt-mark unhold nvidia-tesla-470-kernel-support
